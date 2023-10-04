9b4ff481888ea95e19a51dbdc13533a9bee6ff8d
wearrrrr <rowancole19@gmail.com>
2023-10-04 14:23:21 -0500

    certified amiami moment :tm:

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 import { CurlImpersonate } from "node-curl-impersonate";
 import * as fs from "node:fs";
-let ci = new CurlImpersonate("https://api.amiami.com/api/v1.0/items?pagemax=30&pagecnt=1&lang=eng&s_keywords=touhou%20fumofumo", {
+let ci = new CurlImpersonate("https://api.amiami.com/api/v1.0/items?pagemax=20&lang=eng&mcode=&ransu=&age_confirm=&s_keywords=touhou%20fumofumo", {
     method: "GET",
     flags: ["--ciphers TLS_AES_128_GCM_SHA256,TLS_AES_256_GCM_SHA384,TLS_CHACHA20_POLY1305_SHA256,ECDHE-ECDSA-AES128-GCM-SHA256,ECDHE-RSA-AES128-GCM-SHA256,ECDHE-ECDSA-AES256-GCM-SHA384,ECDHE-RSA-AES256-GCM-SHA384,ECDHE-ECDSA-CHACHA20-POLY1305,ECDHE-RSA-CHACHA20-POLY1305,ECDHE-RSA-AES128-SHA,ECDHE-RSA-AES256-SHA,AES128-GCM-SHA256,AES256-GCM-SHA384,AES128-SHA,AES256-SHA", "--http2", "--http2-no-server-push", "--compressed", "--tlsv1.2", "--alps", "--tls-permute-extensions", "--cert-compression brotli"],
     headers: {
