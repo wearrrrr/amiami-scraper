52f066dc222b27ef743f62750eb453ce59893f6d
wearrrrr <contact@wearr.dev>
2024-11-06 14:23:10 -0600

    Now counts how many fumos are in stock and on preorder, and upgrade node-curl-impersonate to fully use ESM.

--- dist/db.d.ts
+++
@@ -1,3 +0,0 @@
-import { AmiAmiItem } from './types.js';
-export declare function createAmiAmiTable(): Promise<void>;
-export declare function insertAmiAmiData(data: AmiAmiItem): Promise<void>;
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,27 @@
 import { CurlImpersonate } from "node-curl-impersonate";
-import { insertAmiAmiData, createAmiAmiTable } from "./db.js";
-import * as fs from "node:fs"
-import { AmiAmiItem } from "./types.js";
+import { insertAmiAmiData, createAmiAmiTable, getFumosInStock, getFumosForPreorder } from "./db.js";
 
 let ci = new CurlImpersonate("https://api.amiami.com/api/v1.0/items?s_keywords=touhou%20plush&pagecnt=1&pagemax=30&lang=eng", {
   method: "GET",
   headers: {
       "X-User-Key": "amiami_dev"
   },
-  impersonate: "chrome-110",
-}).makeRequest().then(async (data) => {
-    if (data.response == '') {
-        throw new Error("No response from url!")
-    }
-    let json = JSON.parse(data.response)
-    let items = json.items
-    await createAmiAmiTable();
-    items.forEach((item: AmiAmiItem) => {
-        insertAmiAmiData(item)
-    })
+  impersonate: "firefox-109",
 })
-
+const request = await ci.makeRequest()
+if (request.response == "") {
+    throw new Error("No response from url!")
+}
+let json = JSON.parse(request.response)
+let items = json.items;
+let len = items.length;
+await createAmiAmiTable();
+for (let i = 0; i < len; i++) {
+    await insertAmiAmiData(items[i], true)
+};
+let fumosInStock = await getFumosInStock();
+let fumosForPreorder = await getFumosForPreorder();
+console.log(`Inserted ${len} items into the database!`);
+console.log(`There are ${fumosInStock.length} Fumos in stock!`);
+console.log(`There are ${fumosForPreorder.length} Fumos for preorder!`);
 
